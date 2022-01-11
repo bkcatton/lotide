2ae4dfad943fad8e38d1a218e3fab73af4dbc021
bkcatton <ben.k.catton@gmail.com>
2022-01-10 16:40:39 -0800

    Added initial assertEqual function in a .js file.

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  let returnVal;
+  if (actual === expected) {
+    returnVal = "😃Assertion Passed: [" + actual + "] === [" + expected + "]";
+    console.log(returnVal);
+  } else if (actual !== expected) {
+    returnVal = "😔Assertion Failed: [" + actual + "] !== [" + expected + "]";
+    console.log(returnVal);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 5);
