db56366843561c709d3a91bb71ad0dc66cf05ccd
bkcatton <ben.k.catton@gmail.com>
2022-01-13 16:54:18 -0800

    Added a function which takes in an object and searches by property until a match appears(match is checked by a callback function) and then returns the key of the match.

---
+++ findKey.js
@@ -0,0 +1,52 @@
+const findKey = function (object, callback) {
+  obj = object;
+  for (const key in obj) {
+    if(callback(obj[key])) {
+      return key;
+    }
+  };
+}
+const assertEqual = function(actual, expected) {
+  let returnVal;
+  if (actual === expected) {
+    returnVal = `😃Assertion Passed: [${actual}] === [${expected}]`;
+    console.log(returnVal);
+  } else if (actual !== expected) {
+    returnVal = `😔Assertion Failed: [${actual}] !== [${expected}]`;
+    console.log(returnVal);
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, function(x) {
+  return x.stars === 2;
+}), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, function(x) {
+  return x.stars === 1;
+}), "Blue Hill");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, function(x) {
+  return x.stars === 3;
+}), "Akaleri");
+
