763d797ee725be29a5ec7e0a05bf5e1ac7341375
bkcatton <ben.k.catton@gmail.com>
2022-01-17 09:59:35 -0800

    Refactored the eqArrays function to require modules. Added test code in a seperate file.

---
+++ eqArrays.js
@@ -0,0 +1,16 @@
+
+const eqArrays = function (arrayOne, arrayTwo) { //tests if all instances are the same at same index
+  if (arrayTwo.length !== arrayOne.length) {
+    return false;
+  }
+  let testIfEqual = true;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayTwo[i] !== arrayOne[i]) {
+      return false;
+    }
+  }
+  return testIfEqual;
+};
+
+module.exports = eqArrays;
+
---
+++ test/eqArraysTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays')
+//testing if assertEqual works for arrays
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
